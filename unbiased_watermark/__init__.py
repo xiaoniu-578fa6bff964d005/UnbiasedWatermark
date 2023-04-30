d6d517daa3dcf0864bfbef8ec6e53c5ded321896
xiaoniu <hzmxn@mail.ustc.edu.cn>
2023-04-30 11:28:26 -0400

    small change

--- unbiased_watermark/__init__.py
+++ unbiased_watermark/__init__.py
@@ -6,8 +6,8 @@ from .robust_llr import RobustLLR_Score
 from .robust_llr_batch import RobustLLR_Score_Batch
 from .delta import Delta_WatermarkCode, Delta_Reweight
 from .gamma import Gamma_WatermarkCode, Gamma_Reweight
-from .contextcode import *
-from .transformers import *
+from .transformers import WatermarkLogitsProcessor, get_score
+from .contextcode import All_ContextCodeExtractor, PrevN_ContextCodeExtractor
 
 from .test import *
 
