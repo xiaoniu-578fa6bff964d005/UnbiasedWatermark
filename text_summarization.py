e97d0b31882524462771f9e2ca94ef5d22478d4c
Lichang-Chen <Lichang-Chen@github.com>
2023-05-01 20:10:57 -0400

    add watermark

--- text_summarization.py
+++ text_summarization.py
@@ -4,7 +4,7 @@ from transformers import AutoTokenizer
 from transformers import AutoModelForSeq2SeqLM
 import torch
 
-device='cuda'
+device = "cuda"
 # Prepare and tokenize dataset
 cnn_daily = load_dataset("cnn_dailymail", "3.0.0").shuffle(seed=42)
 
@@ -18,33 +18,83 @@ def preprocess_data(example):
     target = example["highlights"]
 
     # Encode the source and target text
-    source_encoding = tokenizer(source, max_length=512, truncation=True, padding="max_length", return_tensors='pt')
-    target_encoding = tokenizer(target, max_length=128, truncation=True, padding="max_length", return_tensors='pt')
+    source_encoding = tokenizer(
+        source,
+        max_length=512,
+        truncation=True,
+        padding="max_length",
+        return_tensors="pt",
+    )
+    target_encoding = tokenizer(
+        target,
+        max_length=128,
+        truncation=True,
+        padding="max_length",
+        return_tensors="pt",
+    )
 
     return {
-        "inputs": source_encoding['input_ids'],
-        "labels": target_encoding['input_ids'],
+        "inputs": source_encoding["input_ids"],
+        "labels": target_encoding["input_ids"],
     }
+
+
 # Apply preprocessing to the dataset
-tokenized_cnn_daily = cnn_daily['test'].map(preprocess_data, batched=True)
+tokenized_cnn_daily = cnn_daily["test"].map(preprocess_data, batched=True)
 
 # Setup evaluation
-rouge = load_metric("rouge")
+#  rouge = load_metric("rouge")
+import evaluate
+
+rouge = evaluate.load("rouge")
 
 
-batch_size = 64
-num_batches = len(tokenized_cnn_daily) // batch_size + 1
+batch_size = 8
+num_batches = len(tokenized_cnn_daily) // batch_size
+num_batches = min(num_batches, 100)
 generated_summaries = []
 
-for i in range(num_batches):
+from tqdm import tqdm
+from unbiased_watermark import (
+    Delta_Reweight,
+    Gamma_Reweight,
+    WatermarkLogitsProcessor,
+    PrevN_ContextCodeExtractor,
+)
+from transformers import (
+    LogitsProcessorList,
+    TemperatureLogitsWarper,
+    set_seed,
+)
+
+delta_wp = WatermarkLogitsProcessor(
+    b"private key",
+    Delta_Reweight(),
+    PrevN_ContextCodeExtractor(5),
+)
+gamma_wp = WatermarkLogitsProcessor(
+    b"private key",
+    Gamma_Reweight(1),
+    PrevN_ContextCodeExtractor(5),
+)
+temperature = 1.0
+
+set_seed(42)
+for i in tqdm(range(num_batches)):
     batch_start = i * batch_size
     batch_end = (i + 1) * batch_size
-    if batch_end < len(tokenized_cnn_daily):
-        batch = tokenized_cnn_daily[batch_start:batch_end]
-    else:
-        batch = tokenized_cnn_daily[batch_start:]
-    batch_summaries = model.generate(torch.Tensor(batch['inputs']).to(device=model.device).long(),
-                             max_length=128)
+    batch = tokenized_cnn_daily[batch_start:batch_end]
+    batch_summaries = model.generate(
+        torch.Tensor(batch["inputs"]).to(device=model.device).long(),
+        max_length=128,
+        do_sample=True,
+        num_beams=1,
+        logits_processor=LogitsProcessorList(
+            #  [TemperatureLogitsWarper(temperature)]
+            [TemperatureLogitsWarper(temperature), delta_wp]
+            #  [TemperatureLogitsWarper(temperature), gamma_wp]
+        ),
+    )
     decodes = tokenizer.batch_decode(batch_summaries, skip_special_tokens=True)
     # The result is for debugging use
     # result = rouge.compute(predictions=batch['highlights'], references=decodes, use_stemmer=True)
@@ -52,11 +102,17 @@ for i in range(num_batches):
     generated_summaries.extend(decodes)
 
 # Calculate the ROUGE scores
-rouge_scores = rouge.compute(predictions=generated_summaries, references=tokenized_cnn_daily["highlights"], rouge_types=["rouge1", "rouge2", "rougeL"])
+rouge_scores = rouge.compute(
+    predictions=generated_summaries,
+    references=tokenized_cnn_daily["highlights"][: len(generated_summaries)],
+    rouge_types=["rouge1", "rouge2", "rougeL"],
+    use_stemmer=True,
+    use_aggregator=False,
+)
 
 print("ROUGE-1: ")
-print(rouge_scores["rouge1"].mid)
+print(np.mean(rouge_scores["rouge1"]))
 print("ROUGE-2: ")
-print(rouge_scores["rouge2"].mid)
+print(np.mean(rouge_scores["rouge2"]))
 print("ROUGE-L: ")
-print(rouge_scores["rougeL"].mid)
+print(np.mean(rouge_scores["rougeL"]))
