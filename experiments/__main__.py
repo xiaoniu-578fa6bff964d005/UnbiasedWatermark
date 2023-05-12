729b125ec7e1b77d272bbf6c86f4b6b2c1aea277
xiaoniu <hzmxn@mail.ustc.edu.cn>
2023-05-12 18:49:47 -0400

    small change

--- experiments/__main__.py
+++ experiments/__main__.py
@@ -3,17 +3,29 @@ from . import machine_translation as mt
 
 
 def add_watermark_exp():
+    print("ts.get_output.pipeline()")
     ts.get_output.pipeline()
+    print("ts.evaluate.pipeline()")
     ts.evaluate.pipeline()
+    print("ts.evaluate_bleu.compute_bleu()")
     ts.evaluate_ppl.pipeline()
 
+    print("mt.get_output.pipeline()")
     mt.get_output.pipeline()
+    print("mt.evaluate.pipeline()")
     mt.evaluate.pipeline()
+    print("mt.evaluate_bleu.compute_bleu()")
     mt.evaluate_bleu.compute_bleu()
+    print("mt.evaluate_ppl.pipeline()")
     mt.evaluate_ppl.pipeline()
 
 
 if __name__ == "__main__":
+    print("Add watermark experiment")
     add_watermark_exp()
+
+    print("Evaluate score experiment")
+    print("ts.evaluate_score.pipeline()")
     ts.evaluate_score.pipeline()
+    print("mt.evaluate_score.pipeline()")
     mt.evaluate_score.pipeline()
